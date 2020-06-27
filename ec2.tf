f0b11daee61fe718a49bf8da007a1df0d5b9b5f1
mojumah <jumah.mohammad@gmail.com>
2020-06-27 23:25:16 +0100

    tf jenkins int

--- ec2.tf
+++ ec2.tf
@@ -1,28 +1,10 @@
 provider "aws" {
-  region = "us-west-2"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "eu-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "example" {
+  ami           = "ami-0cf94b1c148cb4b81"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
